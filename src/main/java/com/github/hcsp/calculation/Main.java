52d59ff920fbe787ebdf5f3bae0eed098c258691
PingAn_NetBar <840495823@qq.com>
2019-08-12 20:51:15 +0800

    实现排序 (#51)
    实现排序 (#51)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.calculation;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     /**
      * 给定三个大小不确定的数字a,b,c，将其按照从大到小的顺序输出。
@@ -13,7 +16,24 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        int ar[] = {a, b, c};
+        for(int j=1;j<ar.length;j++){
+            int k=ar[j];
+            int i=j-1;
+            while(i>=0){
+                if(ar[i]<k){
+                    ar[i+1]=ar[i];
+                    ar[i]=k;
+                }
+                i--;
+            }
+        }
+        return ar[0]+">"+ar[1]+">"+ar[2];
+    }
+       //  还是想着用插入排序试试-v-
+       //  Arrays.sort(ar);
+       //  return ar[2]+">"+ar[1]+">"+ar[0];
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
