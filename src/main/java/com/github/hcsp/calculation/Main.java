feb2281417fba2249620b28acd25671ec6514365
LU YI ZHE <42718757+sceraser@users.noreply.github.com>
2019-08-23 01:02:38 +0800

    Lyz branch (#56)
    Lyz branch (#56)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,27 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        if(a>b&&b>c){
+            return a+">"+b+">"+c;
+        }
+        if (a>b&&a>c&&c>b){
+            return a+">"+c+">"+b;
+        }
+        if(b>a&&a>c){
+            return b+">"+a+">"+c;
+        }
+        if(b>c&&c>a){
+            return b+">"+c+">"+a;
+        }
+        if(c>a&&a>b){
+            return c+">"+a+">"+b;
+        }
+        if (c>b&&b>a){
+            return c+">"+b+">"+a;
+        }
+        return null;
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
