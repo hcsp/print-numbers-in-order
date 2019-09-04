a3be1718810480442d2e4aa080e267ea5ca2f3b3
Mcavoy GENG <gigi834921748@qq.com>
2019-09-04 16:47:02 +0800

    给int排序 (#63)
    给int排序 (#63)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,4 +1,5 @@
 package com.github.hcsp.calculation;
+import java.util.Arrays;
 
 public class Main {
     /**
@@ -13,7 +14,11 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        int intArray[] = {a, b, c};
+        Arrays.sort(intArray);
+        return intArray[2]+">"+intArray[1]+">"+intArray[0];
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
