18ef91631e779dc4d1a74f0a9ca409846b3836f6
ws1603 <48832455+ws1603@users.noreply.github.com>
2019-10-11 12:04:22 +0800

    三个数的排序 (#77)
    三个数的排序 (#77)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,28 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        int temp;
+        if(b>a){
+            temp=a;
+            a=b;
+            b=temp;
+        }
+        if(c>b){
+            if(c>a){
+                temp=c;
+                c=b;
+                b=a;
+                a=temp;
+            }
+            else{
+                temp=b;
+                b=c;
+                c=temp;
+            }
+        }
+        return a+">"+b+">"+c;
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
