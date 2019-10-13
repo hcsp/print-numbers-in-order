ee76037576edbab3278f532475e2ff0a6cabd552
dqprecisian <36305085+dqprecisian@users.noreply.github.com>
2019-10-13 09:07:12 -0500

    按顺序输出数字 (#83)
    按顺序输出数字 (#83)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,27 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+
+        int max = Math.max(Math.max(a, b), c);
+        int min = Math.min(Math.min(a, b), c);
+        int middle = 0;
+
+        if(a < max && a > min) {
+            middle = a;
+        }
+
+        if(b < max && b > min) {
+            middle = b;
+        }
+
+        if(c < max && c > min) {
+            middle = c;
+        }
+
+        return max + ">" + middle + ">" + min;
+
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
