28eaef8d7fac787e40442f614b643703562d8f78
Llane00 <bluetinestyle@126.com>
2019-10-20 16:32:29 +0800

    change: 修改printNumbersInOrder函数，使用数组的sort排序方法从小到大排序数字

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.calculation;
 
+import java.util.Arrays;
+
 public class Main {
     /**
      * 给定三个大小不确定的数字a,b,c，将其按照从大到小的顺序输出。
@@ -14,19 +16,9 @@ public class Main {
      * @return 所要求的字符串
      */
     public static String printNumbersInOrder(int a, int b, int c) {
-        if (a >= b && b >= c) {
-            return a + ">" + b + ">" + c;
-        } else if (a >= c && c >= b) {
-            return a + ">" + c + ">" + b;
-        } else if (b >= a && a >= c) {
-            return b + ">" + a + ">" + c;
-        } else if (b >= c && c >= a) {
-            return b + ">" + c + ">" + a;
-        } else if (c >= a && a >= b) {
-            return c + ">" + a + ">" + b;
-        } else {
-            return c + ">" + b + ">" + a;
-        }
+        int[] arr = {a,b,c};
+        Arrays.sort(arr);
+        return arr[2] + ">" + arr[1] + ">" + arr[0];
     }
 
     public static void main(String[] args) {
