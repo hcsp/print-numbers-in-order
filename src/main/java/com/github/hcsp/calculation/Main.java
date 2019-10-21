991a9ba784cabfa4b19af864c38ba88bb145127c
903875816 <604150318@qq.com>
2019-10-21 17:26:46 +0800

    print-numbers-in-order

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.calculation;
 
+import java.util.Arrays;
+
 public class Main {
     /**
      * 给定三个大小不确定的数字a,b,c，将其按照从大到小的顺序输出。
@@ -13,7 +15,31 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+//        String s;
+//        if (a - b <= 0 && a - c <= 0) {
+//            if (b - c <= 0) {
+//                s = c + ">" + b + ">" + a;
+//            } else {
+//                s = b + ">" + c + ">" + a;
+//            }
+//        } else if (a >= b && a <= c) {
+//            s = c + ">" + a + ">" + b;
+//        } else if (a >= c && a <= b) {
+//            s = b + ">" + a + ">" + c;
+//        } else {
+//            if (b - c >= 0) {
+//                s = a + ">" + b + ">" + c;
+//            } else {
+//                s = a + ">" + c + ">" + b;
+//            }
+//        }
+//        return s;
+        int[] list = new int[]{a,b,c};
+        Arrays.sort(list);
+        String s=list[2]+">"+list[1]+">"+list[0];
+        return s;
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
