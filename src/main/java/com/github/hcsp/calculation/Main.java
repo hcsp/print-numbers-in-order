38af49f8f668589d0d4fc6e2f2dc7bfb6d61ee00
astak16 <1500846601@qq.com>
2019-12-17 10:11:48 +0800

    练习比较运算符的基本操作 (#113)
    练习比较运算符的基本操作 (#113)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.calculation;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Main {
     /**
      * 给定三个大小不确定的数字a,b,c，将其按照从大到小的顺序输出。
@@ -13,7 +16,18 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        Integer[] d = {a, b, c};
+        Arrays.sort(d, Collections.reverseOrder());
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < d.length; i++) {
+            if (i != d.length - 1)
+                str.append(d[i] + ">");
+            else
+                str.append(d[i] + "");
+        }
+        return str + "";
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
