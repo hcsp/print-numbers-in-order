43974b38040e653e2f8d2bcd4ad1f130847d1565
spclock <1124516293@qq.com>
2020-01-02 22:28:18 +0800

    练习比较运算符的基本操作

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,5 +1,9 @@
 package com.github.hcsp.calculation;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     /**
      * 给定三个大小不确定的数字a,b,c，将其按照从大到小的顺序输出。
@@ -14,13 +18,18 @@ public class Main {
      * @return 所要求的字符串
      */
     public static String printNumbersInOrder(int a, int b, int c) {
-        if( a>b && a>c && b>c){ return a+">"+b+">"+c;}
-        else if( a>b && a>c && b<c){ return a+">"+c+">"+b;}
-        else if( b>a && b>c && a>c){ return b+">"+a+">"+c;}
-        else if( b>a && b>c && a<c){ return b+">"+c+">"+a;}
-        else if( c>a && c>b && a>b){ return c+">"+a+">"+b;}
-        //( c>a && c>b && a<b)
-        else { return c+">"+b+">"+a;}
+        //看了别人的代码，发现扩展和维护性太差了
+        List<Integer> list=new ArrayList<>();
+        Collections.addAll(list, a, b,c);
+        Collections.sort(list, Collections.reverseOrder());
+        String order = "";
+
+        for (int  num : list) {
+            //判断是不是最后一个元素，是就不加>
+            if(list.size() - 1 == list.indexOf(num)){ order+=num; }
+            else { order+=num+">";}
+        }
+        return order;
     }
 
     public static void main(String[] args) {
