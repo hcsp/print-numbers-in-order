30f1a29f55fd949e14a7c6a42a20d294059455e5
spclock <1124516293@qq.com>
2020-01-02 21:01:24 +0800

    练习比较运算符的基本操作

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,15 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        if( a>b && a>c && b>c){ return a+">"+b+">"+c;}
+        else if( a>b && a>c && b<c){ return a+">"+c+">"+b;}
+        else if( b>a && b>c && a>c){ return b+">"+a+">"+c;}
+        else if( b>a && b>c && a<c){ return b+">"+c+">"+a;}
+        else if( c>a && c>b && a>b){ return c+">"+a+">"+b;}
+        //( c>a && c>b && a<b)
+        else { return c+">"+b+">"+a;}
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
