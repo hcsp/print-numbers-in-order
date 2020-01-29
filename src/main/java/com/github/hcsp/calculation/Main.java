9f537ca13722684117e1388afde277238ab7496e
Damier <1042413535@qq.com>
2020-01-29 16:42:41 +0800

    排序

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,20 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        int[] arr = new int[]{a, b, c};
+        int temp;
+        for(int i = 0;i < 2;i++){
+            for (int j = 0;j < 2;j++) {
+                if (arr[j] < arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr[0] + ">" + arr[1] + ">" + arr[2];
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
