78200a11658994caa1640ae637418c704f4e8b6e
pathjh <1639925487@qq.com>
2020-02-27 11:30:19 +0800

    比大小

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,24 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        if(a>b){
+            a=a+b;
+            b=a-b;
+            a=a-b;
+        }
+        if(a>c){
+            a=a+c;
+            c=a-c;
+            a=a-c;
+        }
+        if(b>c){
+            b=b+c;
+            c=b-c;
+            b=b-c;
+        }
+        return c+">"+b+">"+a;
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
