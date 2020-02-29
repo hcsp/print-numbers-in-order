4a9d89e3b8962f437d0889ec077538482f924f57
DobbyKim <1175088275@qq.com>
2020-02-29 15:29:17 +0800

    add code

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,36 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        /*int min = a < b ? (a > c ? c : a) : (c < b ? c : a);
+        int max = a > b ? (a > c ? a : b) : (b > c ? b : c);
+        int mid = 0;
+        if (a > min && a < max) {
+            mid = a;
+        } else if (b > min && b < max) {
+            mid = b;
+        } else {
+            mid = c;
+        }
+        return max + " > " + mid + " > " + min;*/
+
+        int[] help = new int[]{a, b, c};
+        for (int i = 1; i < help.length; i++) {
+            for (int j = i; j > 0 && help[j] > help[j - 1]; j--) {
+                swap(help, j, j - 1);
+            }
+        }
+        return help[0] + ">" + help[1] + ">" + help[2];
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        if (i > arr.length - 1 || j > arr.length - 1 || i == j)
+            return;
+
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[j] ^ arr[i];
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
