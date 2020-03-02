31627788fbe69ca5e10afe2c771f6f292a4ba6b6
huiquan99 <53985556+huiquan99@users.noreply.github.com>
2020-03-02 13:59:20 +0800

    compare (#155)
    compare (#155)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,26 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        int temp;
+        if (a > b) {
+            temp = a;
+            a = b;
+            b = temp;
+        }
+        if (b > c) {
+            temp = b;
+            b = c;
+            c = temp;
+        }
+        if (a > b) {
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        return c+">"+b+">"+a;
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
