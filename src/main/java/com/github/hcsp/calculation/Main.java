0015f1a5dbbcfeaa300725f9843b6f2b8f8d53f1
Truthcode <617805327@qq.com>
2020-03-03 15:11:16 +0800

    sss

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -14,21 +14,21 @@ public class Main {
      * @return 所要求的字符串
      */
     public static String printNumbersInOrder(int a, int b, int c) {
-        if (a > b) {
+        if (a > b && a > c) {
             if (b > c) {
                 return a + ">" + b + ">" + c;
             } else {
                 return a + ">" + c + ">" + b;
             }
         }
-        if (a < b) {
+        if (a < b && b > c) {
             if (a > c) {
                 return b + ">" + a + ">" + c;
             } else {
                 return b + ">" + c + ">" + a;
             }
         }
-        if (c < a) {
+        if (c > a && c > b) {
             if (a > b) {
                 return c + ">" + a + ">" + b;
             } else {
@@ -40,6 +40,7 @@ public class Main {
 
 
     public static void main(String[] args) {
+        printNumbersInOrder(1, 2, 3);
         System.out.println(printNumbersInOrder(1, 2, 3));
         System.out.println(printNumbersInOrder(-1, 2, -3));
     }
