b238ba5a65d7a43787d2fd18c1cde02549d57e2f
jslyq <jsfjlyq@gmail.com>
2020-03-06 17:24:08 -0500

    运算符号

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,30 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        if(a>b){
+            if(a>c){
+                if(c>b){
+                    return a+">"+b+">"+c;
+                }
+                else{
+                    return a+">"+c+">"+b;
+                }
+            }
+            else{
+                return c+">"+a+">"+b;
+            }
+        }
+        else if(a>c){
+            return b+">"+a+">"+c;
+        }
+        else if(c<a){
+            return b+">"+c+">"+a;
+        }
+        else{
+            return c+">"+b+">"+a;
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
