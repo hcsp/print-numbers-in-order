13f00148845f6f90ad890fd6120821b9072c3491
liulian1999 <61763395+liulian1999@users.noreply.github.com>
2020-03-11 21:31:25 -0700

    order (#164)
    order (#164)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,30 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        if(a < b && b < c){
+            return c + ">" + b + ">" + a;
+        }
+        else if(a > b && b > c){
+            return a + ">" + b + ">" +c;
+        }
+        else if(b < c && c < a){
+            return a + ">" + c + ">" +b;
+        }
+        else if(a < b && c < a){
+            return b + ">" + a + ">" +c;
+        }
+        else if(c < b && a < c){
+            return b + ">" + c + ">" +a;
+        }
+        else if(c > a && a > b){
+            return c + ">" + a + ">" + b;
+        }
+        else{
+            return " ";
+        }
+
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
