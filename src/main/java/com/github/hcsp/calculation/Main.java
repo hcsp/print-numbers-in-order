508192c63aadee07561d9d97dc131085919afe8b
PudgeWL <nbhaohao@gmail.com>
2020-04-17 01:15:27 +0800

    feat: 完成挑战(改了下，之前没理解到题目的意思) (#188)
    feat: 完成挑战(改了下，之前没理解到题目的意思) (#188)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.calculation;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Main {
     /**
      * 给定三个大小不确定的数字a,b,c，将其按照从大到小的顺序输出。
@@ -13,7 +16,11 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        Integer[] tempArray = {a, b, c};
+        Arrays.sort(tempArray, Collections.reverseOrder());
+        return tempArray[0] + ">" + tempArray[1] + ">" + tempArray[2];
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
