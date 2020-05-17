8e5bd1b1d1432d1592cd8736584d69ec9c05b8fb
hanchenhao <hanchenhao@vip.qq.com>
2020-05-17 10:40:00 +0800

    sort (#199)
    sort (#199)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.calculation;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     /**
      * 给定三个大小不确定的数字a,b,c，将其按照从大到小的顺序输出。
@@ -13,7 +16,12 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        int[] numbers = {a,b,c};
+        Arrays.sort(numbers);
+        return numbers[2] + ">" + numbers[1] + ">" + numbers[0];
+
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
