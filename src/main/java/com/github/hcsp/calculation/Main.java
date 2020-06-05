f1ec19924783bf9a6e596103ce3e675add7a0715
kkeda <kkeda725@gmail.com>
2020-06-05 19:00:57 +0800

    print number in order

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.calculation;
 
+import java.util.Arrays;
+
 public class Main {
     /**
      * 给定三个大小不确定的数字a,b,c，将其按照从大到小的顺序输出。
@@ -13,7 +15,26 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+//        int temp;
+//       if (a > b) {
+//           temp = a;
+//           a = b;
+//           b = temp;
+//       } else if (a > c){
+//           temp = a;
+//           a = c;
+//           c = temp;
+//       } else if (b > c){
+//           temp = a;
+//           a = c;
+//           c = temp;
+//       }
+//       return a+">"+b+">"+c;
+        int[] nums = {a,b,c};
+        Arrays.sort(nums);
+        return nums[2] + ">" + nums[1] +">" + nums[0];
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
