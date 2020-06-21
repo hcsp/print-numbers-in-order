5fcfdde59f9fc0a7088a6cbbc038dc1571e7d030
0x522 <chenyuntao1997@163.com>
2020-06-21 21:20:04 +0800

    比较三个数的大小。

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.calculation;
 
+import java.util.Arrays;
+
 public class Main {
     /**
      * 给定三个大小不确定的数字a,b,c，将其按照从大到小的顺序输出。
@@ -14,26 +16,9 @@ public class Main {
      * @return 所要求的字符串
      */
     public static String printNumbersInOrder(int a, int b, int c) {
-        if (a > b && a > c) {
-            if (b > c) {
-                return a + ">" + b + ">" + c;
-            } else {
-                return a + ">" + c + ">" + b;
-            }
-        } else if (b > a && b > c) {
-            if (a > c) {
-                return b + ">" + a + ">" + c;
-            } else {
-                return b + ">" + c + ">" + a;
-            }
-        } else if (c > a && c > b) {
-            if (a > b) {
-                return c + ">" + a + ">" + b;
-            } else {
-                return c + ">" + b + ">" + a;
-            }
-        }
-        return "error";
+        int[] arr = new int[]{a, b, c};
+        Arrays.sort(arr);
+        return arr[2] + ">" + arr[1] + ">" + arr[0];
     }
 
 
