460f541f2e8d13d14fce3d9ece07f1785fa2d1c8
biantaiwen <biantaiwen@163.com>
2020-08-10 08:38:25 +0800

    fix printNumbersInOrder

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,19 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        int[] numArr = {a, b, c};
+        for (int i = 0; i < numArr.length; i++) {
+            for (int j = 0; j < numArr.length - 1 - i; j++) {
+                if (numArr[j] < numArr[j + 1]) {
+                    int tem = numArr[j];
+                    numArr[j] = numArr[j + 1];
+                    numArr[j + 1] = tem;
+                }
+            }
+        }
+        return "" + numArr[0] + ">" + numArr[1] + ">" + numArr[2];
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
