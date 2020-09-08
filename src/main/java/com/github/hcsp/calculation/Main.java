acb2c6428ef138e90049cbdd8295451549d9d905
Faiz <53805895+faiz-lab@users.noreply.github.com>
2020-09-08 22:01:26 +0800

    完成练习 (#224)
    完成练习 (#224)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,21 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        if (c < b && b < a) {
+            return a + ">" + b + ">" + c;
+        } else if (c < a && b < c) {
+            return a + ">" + c + ">" + b;
+        } else if (c < a && c < b && a < b) {
+            return b + ">" + a + ">" + c;
+        } else if (a < c && a < b && c < b) {
+            return b + ">" + c + ">" + a;
+        } else if (a < b && a < c && b < c) {
+            return c + ">" + b + ">" + a;
+        } else {
+            return c + ">" + a + ">" + b;
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
