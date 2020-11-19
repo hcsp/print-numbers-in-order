9f1c649b65d2337d717e23b8d6b6d9b0e372c7d8
qipengWei <webqipeng@163.com>
2020-11-20 00:33:51 +0800

    排序 修改参数

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,8 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int ...num) {
+    public static String printNumbersInOrder(int a,int b,int c) {
+        int[] num = {a,b,c};
         for(int i = 0; i < num.length - 1; i++) {
             for(int j = 0; j < num.length - 1 - i; j++) {
                 int temp = 0;
