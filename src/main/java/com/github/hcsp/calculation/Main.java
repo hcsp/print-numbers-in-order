4db081cd116f65cd450d5dce87e5682db1c9aebd
HBhakunamatata <59411479+HBhakunamatata@users.noreply.github.com>
2020-12-14 18:48:06 +0800

    how to change the value of two primitive in Java (array? object?) (#235)
    how to change the value of two primitive in Java (array? object?) (#235)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,24 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        if (a < b) {
+            int t = a;
+            a = b;
+            b = t;
+        }
+        if (a < c) {
+            int t = a;
+            a = c;
+            c = t;
+        }
+        if (b < c) {
+            int t = b;
+            b = c;
+            c = t;
+        }
+        return a + ">" + b + ">" + c;
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
