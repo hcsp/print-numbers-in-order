6974d74d08952096c034a6e3e01b32c094dda76a
wong <fanciwong@protonmail.com>
2021-05-06 20:30:16 +0800

    已修正

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.calculation;
 
+import java.util.Arrays;
+
 public class Main {
     /**
      * 给定三个大小不确定的数字a,b,c，将其按照从大到小的顺序输出。
@@ -13,7 +15,19 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        int[] num = {a,b,c};
+        for (int i = 0; i<num.length-1; i++) {
+            for (int j = 0; j < num.length-1-i; j++) {
+                if(num[j]<num[j+1]){
+                    int temp = num[j+1];
+                    num[j+1] = num[j];
+                    num[j] = temp;
+                }
+            }
+        }
+        return num[0]+">"+num[1]+">"+num[2];
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
