4ebd78ab73dda66e8d22744ff852c5d80b70f729
bowen-wu <33862944+bowen-wu@users.noreply.github.com>
2021-06-01 15:14:42 +0800

    Feature/task (#248)
    Feature/task (#248)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.calculation;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Main {
     /**
      * 给定三个大小不确定的数字a,b,c，将其按照从大到小的顺序输出。
@@ -13,7 +16,15 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        Integer[] intArray = {a, b, c};
+        Arrays.sort(intArray, Collections.reverseOrder());
+        StringBuilder result = new StringBuilder();
+        for (Integer number : intArray) {
+            result.append(number).append(">");
+        }
+        return result.substring(0, result.length() - 1);
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
