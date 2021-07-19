74e6b4aed32cac5d1bb6dc21720fe3cd61b7ccd5
simonsyang <75574231+Vcsimonsyang@users.noreply.github.com>
2021-07-19 16:30:42 +0800

    sorted the given numbers and print them in given order. (#255)
    sorted the given numbers and print them in given order. (#255)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,25 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        int temp;
+        String string;
+        int[] order = new int[3];
+        order[0] = a;
+        order[1] = b;
+        order[2] = c;
+        for (int i = 0; i < order.length - 1; i++) {
+            for (int j = 0; j < order.length - 1 - i; j++) {
+                if (order[j] < order[j + 1]) {
+                    temp = order[j];
+                    order[j] = order[j + 1];
+                    order[j + 1] = temp;
+                }
+            }
+        }
+        string = order[0] + ">" + order[1] + ">" + order[2];
+        return string;
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
