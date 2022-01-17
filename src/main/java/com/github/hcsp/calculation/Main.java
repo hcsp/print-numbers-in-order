58254e6e200d2b806c67af7deedcb3ae9e905689
Hao Jing <92510575+hitjh@users.noreply.github.com>
2022-01-17 21:21:54 +0800

    排序 (#279)
    排序 (#279)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,12 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        int maxNumber = Math.max(Math.max(a, b), Math.max(b, c));
+        int minNumber = Math.min(Math.min(a, b), Math.min(b, c));
+        int midNumber = maxNumber ^ minNumber ^ a ^ b ^ c;
+        return maxNumber + ">" + midNumber + ">" + minNumber;
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
