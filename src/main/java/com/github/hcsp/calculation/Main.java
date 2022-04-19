fc983d1539830c6ac29f60ed280e769f76ace5e5
XT <871963417@qq.com>
2022-04-19 11:56:42 +0800

    Use ternary operator to find the max and min number, done.

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,13 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        int max, mid, min;
+        max=(a>b?a:b)>c?(a>b?a:b):c;
+        min=(a<b?a:b)<c?(a<b?a:b):c;
+        mid=a+b+c-max-min;
+        return max+">"+mid+">"+min;
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
