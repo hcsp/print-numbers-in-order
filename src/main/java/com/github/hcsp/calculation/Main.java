657c4275cabce857f712e2491cb47d3b5da1a0a4
pageye123 <908202572@qq.com>
2022-06-02 20:54:43 +0800

    练习比较运算符的基本操作(方法二)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -1,33 +1,6 @@
 package com.github.hcsp.calculation;
 
 public class Main {
-    private static int max;
-
-    private static String compare(int a, int b) {
-        if (a > b) {
-            return a + ">" + b;
-        } else {
-            return b + ">" + a;
-        }
-    }
-
-    private static char setMax(int a, int b, int c) {
-        char maxV;
-        if (a > b) {
-            max = a;
-            maxV = 'a';
-        } else {
-            max = b;
-            maxV = 'b';
-        }
-        if (max < c) {
-            max = c;
-            maxV = 'c';
-        }
-        return maxV;
-
-    }
-
     /**
      * 给定三个大小不确定的数字a,b,c，将其按照从大到小的顺序输出。
      *
@@ -41,21 +14,29 @@ public class Main {
      * @return 所要求的字符串
      */
     public static String printNumbersInOrder(int a, int b, int c) {
-        char maxV = Main.setMax(a, b, c);
-        String postfix = null;
-        switch (maxV) {
-            case 'a':
-                postfix = compare(b, c);
-                break;
-            case 'b':
-                postfix = compare(a, c);
-                break;
-            case 'c':
-                postfix = compare(a, b);
-                break;
+        if (a > b) {
+            if (a > c) {
+                if (b > c) {
+                    return a + ">" + b + ">" + c;
+                } else {
+                    return a + ">" + c + ">" + b;
+                }
+            } else {//a<=c
+                return c + ">" + a + ">" + b;
+            }
+        }else {//a<=b
+            if(b>c){
+                if(a>c){
+                    return b+">"+a+">"+c;
+                }else{
+                    return b+">"+c+">"+a;
+                }
+            }else{//b<=c
+                return c+">"+b +">"+a;
+            }
         }
-        return max + ">" + postfix;
     }
+
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
