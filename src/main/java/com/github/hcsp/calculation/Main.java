f076c74ba3997e269eccdb084fc69eccf5006ef6
Ye Shiqing <908202572@qq.com>
2022-06-02 20:56:21 +0800

    练习比较运算符的基本操作(方法二) (#298)
    练习比较运算符的基本操作(方法二) (#298)

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,30 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        if (a > b) {
+            if (a > c) {
+                if (b > c) {
+                    return a + ">" + b + ">" + c;
+                } else {
+                    return a + ">" + c + ">" + b;
+                }
+            } else {//a<=c
+                return c + ">" + a + ">" + b;
+            }
+        }else {//a<=b
+            if(b>c){
+                if(a>c){
+                    return b+">"+a+">"+c;
+                }else{
+                    return b+">"+c+">"+a;
+                }
+            }else{//b<=c
+                return c+">"+b +">"+a;
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
