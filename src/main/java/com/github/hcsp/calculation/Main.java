fdee99523ae4c0a05fd16b3b016a7e159af827cc
Leo Cheung <leozhang621@gmail.com>
2023-12-06 18:22:29 +0800

    feat: completed missions

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,16 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        int max = Math.max(Math.max(a, b), c);
+        int min = Math.min(Math.min(a, b), c);
+        int middle = a + b + c - max - min;
+        return buildAnswer(max, middle, min);
+    }
+
+    public static String buildAnswer(int a, int b, int c) {
+        return a + ">" + b + ">" + c;
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
