39c6a7b9cd5b424b6e5ade6d53f51695d0f222e1
buuoltwo <bullnick20@gmail.com>
2024-07-25 13:41:10 +0800

    0

--- src/main/java/com/github/hcsp/calculation/Main.java
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -13,7 +13,15 @@ public class Main {
      * @param c 数字三
      * @return 所要求的字符串
      */
-    public static String printNumbersInOrder(int a, int b, int c) {}
+    public static String printNumbersInOrder(int a, int b, int c) {
+        int max = Math.max(Math.max(a, b), c);
+        int min = Math.min(Math.min(a, b), c);
+        int middle = a + b + c - max - min;
+        return toResult(max, middle, min);
+    }
+    private static String toResult(int a, int b, int c) {
+        return a + ">" + b + ">" + c;
+    }
 
     public static void main(String[] args) {
         System.out.println(printNumbersInOrder(1, 2, 3));
